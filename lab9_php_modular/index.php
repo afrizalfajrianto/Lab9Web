89d365cde0f8f10c494526d1a2e2d11fa9b88d69
afrizalfajrianto <kongsafrizal@gmail.com>
2023-11-28 11:50:20 +0700

    Upload File

---
+++ lab9_php_modular/index.php
@@ -0,0 +1,60 @@
+<?php
+include("koneksi.php");
+require('header.php');
+
+// Query to retrieve data
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Data Barang</title>
+</head>
+<body>
+    <div class="container">
+        <h1>DATA BARANG</h1>
+        <div class="main">
+            <a href="tambah.php">Tambah Barang</a>
+            <table>
+                <!-- Table header -->
+                <tr>
+                    <th>Gambar</th>
+                    <th>Nama Barang</th>
+                    <th>Kategori</th>
+                    <th>Harga Jual</th>
+                    <th>Harga Beli</th>
+                    <th>Stok</th>
+                    <th>Aksi</th>
+                </tr>
+
+                <!-- Display data from the database -->
+                <?php if($result && mysqli_num_rows($result) > 0): ?>
+                    <?php while($row = mysqli_fetch_array($result)): ?>
+                        <tr>
+                            <td><img src="gambar/<?= $row['gambar'];?>" alt="<?= $row['nama'];?>"></td>
+                            <td><?= $row['nama'];?></td>
+                            <td><?= $row['kategori'];?></td>
+                            <td><?= $row['harga_beli'];?></td>
+                            <td><?= $row['harga_jual'];?></td>
+                            <td><?= $row['stok'];?></td>
+                            <td>
+                                <a href="ubah.php?id=<?= $row['id_barang'];?>">Ubah</a>
+                                <a href="hapus.php?id=<?= $row['id_barang'];?>">Hapus</a> 
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                <?php else: ?>
+                    <tr>
+                        <td colspan="7">Belum ada data</td>
+                    </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+    <?php require('footer.php')?>
+</body>
+</html>
